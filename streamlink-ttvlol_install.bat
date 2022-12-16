d73b90fff97b703623e6bda531401be3b44bd4dd
Ryun Grivois <ryungrivois@gmail.com>
2022-12-16 15:43:09 -0500

    batch install

---
+++ streamlink-ttvlol_install.bat
@@ -0,0 +1,26 @@
+:: Name:     streamlink-ttvlol_install.bat
+:: Purpose:  Copies the twitch.py file to streamlinks AppData dir
+:: Author:   https://steamcommunity.com/id/sokoloft/
+:: Revision: December 16/22
+:: Notes: 	 
+
+@echo off
+title streamlink-ttvlol_install
+cd "%AppData%"
+if exist streamlink (
+	cd streamlink
+	mkdir plugins
+	cd /d "%~dp0"
+	xcopy /q /y ".\twitch.py" "%AppData%\streamlink\plugins"
+	cls
+	color 0a
+	echo twitch.py copied successfully.
+	echo This window will now close.
+	TIMEOUT 5
+	exit
+) else (
+	color 0c
+	echo Error.
+	echo Unable to find streamlink AppData directory.
+	pause
+)
