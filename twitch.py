1cf5d765e85861ec82ca6a9d2a6fe1526ec7acbb
2bc4 <affett0@proton.me>
2024-03-19 20:42:10 -0500

    6.7.1-20240320

--- twitch.py
+++ twitch.py
@@ -53,7 +53,7 @@ from streamlink.utils.url import update_qsd
 log = logging.getLogger(__name__)
 
 LOW_LATENCY_MAX_LIVE_EDGE = 2
-STREAMLINK_TTVLOL_VERSION = "90fe9e42-master"
+STREAMLINK_TTVLOL_VERSION = "6.7.1-20240320"
 
 
 @dataclass
